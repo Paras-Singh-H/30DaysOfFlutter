ccd8499df1066f85f0bbe04fae0906f53fdf2487
Paras <singhparas3@gmail.com>
2021-02-23 04:23:59 +0530

    Day 2 completed

---
+++ lib/home_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  final int days = 30;
+  final String name = "Paras";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catelog App"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Welcome to $days of Flutter by $name !!!"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
