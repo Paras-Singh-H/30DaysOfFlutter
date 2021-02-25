1713cfe4f7c3944c407c7e9e91ca57f655e68b07
Paras <singhparas3@gmail.com>
2021-02-25 15:03:37 +0530

    Day 12 completed

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,5 +1,18 @@
+class CatalogModel {
+  static final items = [
+    Item(
+        id: 1,
+        name: "Iphone 12 Pro",
+        desc: "Apple iphone 12th generation",
+        price: 900,
+        colors: "#33505a",
+        image:
+            "https://images.idgesg.net/images/article/2020/11/iphone-12-pro-max-size-100866257-large.jpg")
+  ];
+}
+
 class Item {
-  final String id;
+  final int id;
   final String name;
   final String desc;
   final num price;
@@ -8,14 +21,3 @@ class Item {
 
   Item({this.id, this.name, this.desc, this.price, this.colors, this.image});
 }
-
-final products = [
-  Item(
-      id: "Codepur001",
-      name: "Iphone 12 Pro",
-      desc: "Apple iphone 12th generation",
-      price: 900,
-      colors: "#33505a",
-      image:
-          "https://images.idgesg.net/images/article/2020/11/iphone-12-pro-max-size-100866257-large.jpg")
-];
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,22 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/models/catalog.dart';
 import 'package:flutter_catalog/widgets/drawer.dart';
-
+import 'package:flutter_catalog/widgets/item_widget.dart';
 
 //Day 11 we learned bout context and constraints.
-
 
 class HomePage extends StatelessWidget {
   final int days = 30;
   final String name = "Paras";
   @override
   Widget build(BuildContext context) {
+    final dummyList = List.generate(20, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: Text("Catelog App"),
       ),
-      body: Center(
-        child: Container(
-          child: Text("Welcome to $days Days Of Flutter by $name !!!"),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount: dummyList.length,
+          itemBuilder: (context, index) {
+            return ItemWidget(
+              item: dummyList[index],
+            );
+          },
         ),
       ),
       drawer: MyDrawer(),
